b225a6a73d53b9a843257ab5a6c4764ba75ac65b
daehan <dh16931@gmail.com>
2022-09-02 18:09:48 +0900

    feat: 뉴럴웍스 추가

--- client/src/components/about/AboutMe.tsx
+++ client/src/components/about/AboutMe.tsx
@@ -11,13 +11,10 @@ export default function AboutMe() {
     return (
         <Box sx={{ width: { mobile: '90%', tablet: '70%', laptop: '50%' }, textAlign: 'left', margin: '10px auto' }}>
             <Typography variant="body1" mb={2}>
-                {translate('AboutOne')}
+                {translate('AboutNeuralworks')}
             </Typography>
             <Typography variant="body1" mb={2}>
-                {translate('AboutTwo')}
-            </Typography>
-            <Typography variant="body1" mb={2}>
-                {translate('AboutThree')}
+                {translate('AboutAcryl')}
             </Typography>
         </Box>
     );
--- client/src/components/work/Companies.tsx
+++ client/src/components/work/Companies.tsx
@@ -13,6 +13,12 @@ interface ICompany {
 
 const companies: ICompany[] = [
     {
+        name: 'Neuralworks',
+        homepage: 'https://neuralworks.io/',
+        date: '2022-03-16 ~ ',
+        tasks: ['Web Backend developer', 'NestJS', 'TDD(E2E, Intergation, Unit)', 'MongoDB(Mongoose), Lambda'],
+    },
+    {
         name: 'Acryl',
         homepage: 'https://www.acryl.ai/',
         date: '2019-08-12 ~ 2021-02-26',
@@ -24,7 +30,7 @@ export default function Companies() {
     return (
         <Box sx={{ width: { mobile: '100%', tablet: 500, laptop: 1000 }, margin: '30px auto' }}>
             {companies.map(company => (
-                <Box key={company.name} sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+                <Box key={company.name} sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', mb: 10 }}>
                     <Box sx={{ width: 200 }}>
                         <Link href={company.homepage} color="primary.800">
                             <Typography variant="h4">{company.name}</Typography>
